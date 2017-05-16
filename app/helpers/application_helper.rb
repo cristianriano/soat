c1520eef09ebacd571be0376e4539f60aaf60a5e
Cristian Riaño <cristiancarm11@gmail.com>
2017-05-16 14:39:54 -0500

    Add carousel to home page. Create helper for titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Set HTML title on head tag
+  def full_title(page_title = '')
+    base_title = "SOAT"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
