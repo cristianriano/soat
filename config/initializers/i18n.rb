2b1462841928bffc647aaf9c4f49ce980cbe9c96
Cristian Riaño <cristian.riano@codescrum.com>
2017-05-17 10:18:51 -0500

    Translate all static pages

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,1 +1,6 @@
+# Available locales and default one
 I18n.config.available_locales = [:en, :es]
+I18n.config.default_locale = :es
+
+# The default locale is :es and all translations from config/locales/*.rb,yml are auto loaded.
+I18n.config.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
