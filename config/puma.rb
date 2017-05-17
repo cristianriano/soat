f26e829879cfd4910d444afc4e57ca4403e43dc0
Cristian Riaño <cristiancarm11@gmail.com>
2017-05-16 20:24:22 -0500

    Setup heroku deploy

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 workers Integer(ENV['WEB_WORKERS'] || 2)
 # Time it waits for the connection to ends. In development increase for pry
 worker_timeout Integer(ENV['WORKER_TIMEOUT'] || 30)
-max_threads = Integer(ENV['MAX_THREADS'] || 16)
-min_threads = Integer(ENV['MIN_THREADS'] || 0)
+max_threads = Integer(ENV['MAX_THREADS'] || 8)
+min_threads = Integer(ENV['MIN_THREADS'] || 8)
 threads min_threads, max_threads
 
 pidfile 'tmp/pids/puma.pid'
