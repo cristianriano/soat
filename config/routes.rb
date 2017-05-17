a7c070cfa9e27ee558489185a0df9582c0d9984e
Cristian Riaño <cristiancarm11@gmail.com>
2017-05-16 20:00:51 -0500

    Configure Devise. Create User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   root to: 'pages#home'
 
   get 'home'          => 'pages#home', as: :home
