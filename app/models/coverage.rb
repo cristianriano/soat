22bff42089b5a5cadb06aada01abab092ca3d7e0
Cristian Riaño <cristiancarm11@gmail.com>
2017-05-19 00:48:50 -0500

    Show coverage on show insurance. Add SOAT summary page on dashboard

--- app/models/coverage.rb
+++ app/models/coverage.rb
@@ -1,4 +1,27 @@
 class Coverage < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
+  TYPES = [:death, :medical, :inability, :transport]
+  DESCRIPTIONS = {death: "Muerte y gastos funerarios",
+    medical: "Gastos médicos",
+    inability: "Incapacidad permanente",
+    transport: "Gastos de transporte"}
+
   acts_as_singleton
 
+  # Methods
+  def death_coverage
+    "#{number_to_currency(death, delimiter: '.', precision: 0)} #{currency}"
+  end
+
+  def medical_coverage
+    "#{number_to_currency(medical, delimiter: '.', precision: 0)} #{currency}"
+  end
+
+  def inability_coverage
+    "#{number_to_currency(inability, delimiter: '.', precision: 0)} #{currency}"
+  end
+
+  def transport_coverage
+    "#{number_to_currency(transport, delimiter: '.', precision: 0)} #{currency}"
+  end
 end
