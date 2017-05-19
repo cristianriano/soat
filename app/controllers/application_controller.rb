65856ecbc78b39d5b877ab1b4f3265dc0c3b7703
Cristian Riaño <cristiancarm11@gmail.com>
2017-05-18 19:59:08 -0500

    Add general resque for NotFound and NotUnique

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_in_use
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   protect_from_forgery with: :exception
   before_action :set_locale
 
@@ -14,5 +17,15 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user != nil
+  end
+
+  def record_in_use
+    flash[:danger] = "Los datos estan en uso"
+    redirect_to :back
+  end
+
+  def record_not_found
+    flash[:danger] = "Lo sentimos. No se encontro la información que estas buscando"
+    redirect_to :back
   end
 end
